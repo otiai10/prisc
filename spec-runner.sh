0b1c9f2e3dfa54751a30cdb97d4e5eb14edbad50
otiai10 <otiai10@gmail.com>
2014-04-09 00:27:43 +0900

    [?] testem launchers?

--- spec-runner.sh
+++ spec-runner.sh
@@ -5,11 +5,12 @@ TAP_FILE='report.tap'
 source ~/.nvm/nvm.sh
 nvm use 0.10
 
-git config url."https://".insteadOf git://
+git config url."https://".insteadOf "git://"
 
 npm install
 bower install
 
 grunt clean
 grunt test
+testem launchers
 testem ci > $TAP_FILE
