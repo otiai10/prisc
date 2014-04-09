37a7c787852bc7215c4c8fedb05d48826dfcf972
otiai10 <otiai10@gmail.com>
2014-04-09 15:02:32 +0900

    [?] State API

--- spec-runner.sh
+++ spec-runner.sh
@@ -2,6 +2,10 @@
 
 TAP_FILE='report.tap'
 
+OWNER=$1
+REPO=$2
+SHA=$3
+
 source ~/.nvm/nvm.sh
 export PATH=$PATH:~/.phantomjs/bin
 nvm use 0.10
@@ -20,8 +24,11 @@ NG_COUNT=`cat $TAP_FILE | grep "not ok" | wc -l`
 
 if [ $NG_COUNT -eq 0 ] ; then
   COMMENT="OK"
+  STATE="success"
 else
   COMMENT=":shit: NG"
+  STATE="failure"
 fi
 
-curl -H "Authorization: token cf2656ece6c8138c73deb4622d961568cc08d444" https://api.github.com/repos/otiai10/prisc/issues/25/comments -X POST -d "{\"body\":\"$COMMENT\"}"
+curl -H "Authorization: token cf2656ece6c8138c73deb4622d961568cc08d444" https://api.github.com/repos/$OWNER/$REPO/issues/25/comments -X POST -d "{\"body\":\"$COMMENT\"}"
+curl -H "Authorization: token cf2656ece6c8138c73deb4622d961568cc08d444" https://api.github.com/repos/$OWNER/$REPO/statuses/$SHA -X POST -d "{\"state\":\"$STATE\"}"
