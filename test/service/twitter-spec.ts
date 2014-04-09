17431331a4e2dd377807eef95eb83fe8f3dd70bd
otiai10 <otiai10@gmail.com>
2014-04-09 14:12:06 +0900

    Modify tests for exam spec-runner

--- test/service/twitter-spec.ts
+++ test/service/twitter-spec.ts
@@ -8,7 +8,19 @@ module Spec {
 
     describe('ServiceTwitter', () => {
         it('should be true', () => {
-            true.should.be.false;
+            true.should.be.true;
         });
     });  
+
+    describe('Sample00', () => {
+        it('should be fail on purpose', () => {
+            true.should.be.false;
+        });
+    });
+
+    describe('Sample01', () => {
+        it('should be success on purpose', () => {
+            true.should.be.true;
+        });
+    });
 }
