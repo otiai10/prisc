ef61bfe3553a0be390d8fb02b0baa2f6442da467
otiai10 <otiai10@gmail.com>
2014-04-09 14:40:49 +0900

    Comment try

--- spec-runner.sh
+++ spec-runner.sh
@@ -15,3 +15,13 @@ grunt clean
 grunt test
 testem launchers
 testem ci > $TAP_FILE
+
+NG_COUNT=`cat $TAP_FILE | grep "not ok" | wc -l`
+
+if [ $NG_COUNT -eq 0 ] ; then
+  COMMENT_BODY="OK"
+else
+  COMMENT_BODY=":shit: NG"
+fi
+
+curl -H "Authorization: token cf2656ece6c8138c73deb4622d961568cc08d444" https://api.github.com/repos/otiai10/prisc/issues/25/comments -X POST -d "{\"body\":\"Hi\"}"
